6120d9b855096ce1991d5b1958d9b5546759bdb3
Rastko Simic <rastko.simic@alfatraining.com>
2024-08-14 12:42:08 +0200

    Taylored postgres database for the future Database per Service use.

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE stillfresh;
-CREATE USER stillfreshuser WITH PASSWORD 'FreshStill011';
-GRANT ALL PRIVILEGES ON DATABASE stillfresh TO stillfreshuser;
-ALTER USER stillfreshuser WITH PASSWORD 'FreshStill011';
+CREATE DATABASE stillfresh_userdb;
+CREATE USER stillfreshuser WITH PASSWORD 'FreshStill011User';
+GRANT ALL PRIVILEGES ON DATABASE stillfresh_userdb TO stillfreshuser;
+ALTER USER stillfreshuser WITH PASSWORD 'FreshStill011User';
